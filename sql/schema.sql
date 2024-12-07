36a7c678cd8e1b5fbb3102ed705a17aa79d14ea8
AlexandreBrakel <xandrebwpg@gmail.com>
2024-12-06 20:57:27 -0600

    move files around

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,26 +1,30 @@
 CREATE TABLE Person(
     personID INT NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name VARCHAR(150) NOT NULL,
     age INT,
     birthYear INT,
     deathYear INT
 );
+
 CREATE TABLE Title(
     titleID INT NOT NULL PRIMARY KEY,
-    primaryTitle TEXT NOT NULL,
-    originalTitle TEXT NOT NULL,
+    primaryTitle VARCHAR(200) NOT NULL,
+    originalTitle VARCHAR(200) NOT NULL,
     startYear INT,
     runTime INT CHECK(runTime>0),
     isAdult BIT NOT NULL
 );
+
 CREATE TABLE Profession(
     professionName VARCHAR(150) PRIMARY KEY
 );
+
 CREATE TABLE HasProfession(
     personID INT REFERENCES Person ON DELETE CASCADE,
     professionName VARCHAR(150) FOREIGN KEY REFERENCES Profession ON DELETE CASCADE,
     PRIMARY KEY (personID, professionName)
 );
+
 CREATE TABLE AssociatedWith(
     personID INT FOREIGN KEY REFERENCES Person ON DELETE CASCADE,
     titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
@@ -28,6 +32,7 @@ CREATE TABLE AssociatedWith(
     CHECK (flag in ('KnownFor', 'Directed', 'Wrote')),
     PRIMARY KEY (personID, titleID, flag)
 );
+
 CREATE TABLE WorksOn(
     personID INT FOREIGN KEY REFERENCES Person ON DELETE CASCADE,
     titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
@@ -35,37 +40,26 @@ CREATE TABLE WorksOn(
     jobName VARCHAR(200),
     PRIMARY KEY (personID, titleID, jobCategory, jobName) 
 );
-CREATE TABLE Directs(
-    personID INT FOREIGN KEY REFERENCES Person ON DELETE CASCADE,
-    titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
-    PRIMARY KEY (personID, titleID) 
-);
-CREATE TABLE Writes(
-    personID INT FOREIGN KEY REFERENCES Person ON DELETE CASCADE,
-    titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
-    PRIMARY KEY (personID, titleID) 
-);
+
 CREATE TABLE ActsIn(
     personID INT FOREIGN KEY REFERENCES Person ON DELETE CASCADE,
     titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
     characterPlayed VARCHAR(200),
     PRIMARY KEY (personID, titleID, characterPlayed)
 );
-CREATE TABLE KnownFor(
-    personID INT  FOREIGN KEY REFERENCES Person ON DELETE CASCADE,
-    titleID INT  FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
-    PRIMARY KEY (personID, titleID) 
-);
+
 CREATE TABLE TitleRating(
     titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
     avgRating NUMERIC(3,1) NOT NULL,
     numVotes INT NOT NULL,
     PRIMARY KEY (titleID, avgRating,numVotes)
 );
+
 CREATE TABLE TVSeries(
     seriesID INT PRIMARY KEY,
     titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE
 );
+
 CREATE TABLE TVEpisode(
     episodeID INT IDENTITY(1, 1),
     titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
@@ -74,6 +68,7 @@ CREATE TABLE TVEpisode(
     episodeNum INT,
     PRIMARY KEY (episodeID, titleID, seriesID)
 );
+
 CREATE TABLE Movie(
     movieID INT IDENTITY(1, 1),
     titleID INT FOREIGN KEY REFERENCES Title ON DELETE CASCADE,
